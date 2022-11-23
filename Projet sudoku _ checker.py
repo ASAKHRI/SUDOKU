3d5caf92fcc191924d7e5d4377eec38e15b1410b
NaNais <merien.nais@gmail.com>
2022-11-23 13:41:08 +0100

    concat final

--- Projet sudoku _ checker.py
+++ Projet sudoku _ checker.py
@@ -1,23 +1,39 @@
 import numpy as np
 import pandas as pd
 
-
+# Creation of DataFrames
 a=np.random.randint(1,10, size=(3,3)) 
 a_df = pd.DataFrame(a)
-a_df = a_df.loc[~a_df.index.duplicated(keep='first')]
-a_df
-
 b=np.random.randint(1,10, size=(3,3))
 b_df = pd.DataFrame(b)
-b_df
-
 c=np.random.randint(1,10, size=(3,3))
 c_df = pd.DataFrame(c)
-c_df[0].is_unique
-
 d=np.random.randint(1,10, size=(3,3))
 d_df = pd.DataFrame(d)
 e=np.random.randint(1,10, size=(3,3))
 e_df = pd.DataFrame(e)
 f=np.random.randint(1,10, size=(3,3))
 f_df = pd.DataFrame(f)
+g=np.random.randint(1,10, size=(3,3))
+g_df = pd.DataFrame(g)
+h=np.random.randint(1,10, size=(3,3))
+h_df = pd.DataFrame(h)
+i=np.random.randint(1,10, size=(3,3))
+i_df = pd.DataFrame(i)
+
+
+#
+concat_a_b = pd.concat([a_df,b_df],axis=1,ignore_index = True)
+concat_a_b_c = pd.concat([concat_a_b,c_df],axis=1,ignore_index=True)
+concat_a_b_c_d = pd.concat([d_df,e_df,f_df],axis=1,ignore_index=True)
+concat_a_b_c_d=concat_a_b_c_d.reset_index(drop=True)
+concat_a_b_c_d_1 = pd.concat([concat_a_b_c,concat_a_b_c_d],axis=0,ignore_index=True)
+concat_g_h_i = pd.concat([g_df,h_df,i_df],axis=1,ignore_index=True)
+
+
+concat_a_b_c_d = concat_a_b_c_d.loc[~concat_a_b_c_d.index.duplicated(keep='first')]
+concat_g_h_i = concat_g_h_i.loc[~concat_g_h_i.index.duplicated(keep='first')]
+
+concat_final = pd.concat([concat_a_b_c_d_1,concat_g_h_i],axis=0,ignore_index=True)
+print(concat_final)
+
