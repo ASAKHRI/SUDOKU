47790cd41753890b769cf7b65871e034f181de70
maxime lu <merdeth69@gmail.com>
2022-12-05 13:47:13 +0100

    checker

--- sudoku_checker.py
+++ sudoku_checker.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def verify(grid):
+def verify1(grid):
 
     lign = grid[:,0]
     colon = grid[0,:]
@@ -18,6 +18,27 @@ def verify(grid):
     for i in grid:
 
         if len(set(i)) != len(lign) and len(set(i)) != len(colon) and len(set(i)) != len(case1) and len(set(i)) != len(case2) and len(set(i)) != len(case3)and len(set(i)) != len(case4) and len(set(i)) != len(case5) and len(set(i)) != len(case6)and len(set(i)) != len(case7) and len(set(i)) != len(case8) and len(set(i)) != len(case9):
+            val += 1
+    if val == 0:
+        return True
+    else:
+        return False
+
+def verify2(grid):
+
+    lign = grid[:,0]
+    column = grid[0,:]
+    squares = []
+    val = 0
+
+    for i in range(0,9,3):
+        for j in range(0,9,3):
+            square = list(np.ravel(row[j:j+3] for row in grid[i:i+3]))
+            squares.append(square)
+    if len(squares) != 9:
+        val += 1
+    for i in grid:
+        if len(set(i)) != len(lign) and len(set(i)) != len(column):
             val += 1
     if val == 0:
         return True
